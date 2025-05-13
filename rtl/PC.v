5253bf0b6e64b713480338c794654947a593bee8
Sowmith Madoori <137033648+sowmith12@users.noreply.github.com>
2025-05-13 18:37:05 +0530

    Create PC.v

---
+++ rtl/PC.v
@@ -0,0 +1,12 @@
+module PC (
+    input clk, rst,
+    input [31:0] pc_i,
+    output reg [31:0] pc_o
+);
+    always @(posedge clk) begin
+        if (~rst)
+            pc_o <= 32'b0;
+        else
+            pc_o <= pc_i;
+    end
+endmodule
